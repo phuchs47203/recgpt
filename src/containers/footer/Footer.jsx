6349c8d5bb1eda12ed1e00409095b6c90d4b2448
phuchs47203 <21522479@gm.uit.edu.vn>
2023-07-29 19:46:09 +0700

    edited

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -45,7 +45,7 @@ const Footer = () => {
       </div>
       <div className='gpt3__footer-copyright'>
         <p>
-          © 2021 GPT-3. All rights reserved.
+          © 2023 GPT-3. All rights reserved.
         </p>
       </div>
     </div>
